d39a58e49a9e20c37f52b9b6d696e02dcc96abb4
Viktoria Domashchuk <domashchukviktoria@gmail.com>
2022-11-09 20:09:59 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,27 @@
 // Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, 
-//використовуючи інлайн стиль.Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+// використовуючи інлайн стиль.Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
 // УВАГА
 // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
 //Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною(disabled).
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+    color: document.querySelector('body'),
+};
+
+let onStart = null;
+// refs.start.addEventListener('click', onStop);
+
+refs.start.addEventListener("click", () => {
+    onStart = setInterval(() => {
+        refs.color.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
